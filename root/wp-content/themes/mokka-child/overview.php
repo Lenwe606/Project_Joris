4222ffaf2f866e133bfc46086582f3ad6e9806a6
Alisio Putman <alisio.putman@student.howest.be>
2015-06-01 10:38:13 +0200

    update css
    update css

--- root/wp-content/themes/mokka-child/overview.php
+++ root/wp-content/themes/mokka-child/overview.php
@@ -5,15 +5,13 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title>Groene Straat</title>
-    <!--<link href="css/layout.css" rel="stylesheet"/>-->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>-child/css/layout.css" media="screen" />
     <!--TODO Meta tags-->
 </head>
 <body>
 
-
 <div id="wrapper">
     <div id="content">
-
         <section id="news" class="section_header">
             <h1 class="section_name"><?php echo $news; ?></h1>
             <ul><?php echo news_limitid(get_news(), $max_shown_news); ?></ul>
@@ -22,15 +20,26 @@
         <section id="events" class="section_header">
             <h1 class="section_name"><?php echo $future_events; ?></h1>
             <ul><?php echo events_limited(get_new_events($max_shown_events), $max_shown_events); ?></ul>
-            <p class="more_items"><a href="eventoverview.php"><?php echo $more_events; ?></a></p>
+            <?php
+            if (count(get_events()) > $max_shown_events) {
+                ?>
+                <p class="more_items"><a href="eventoverview.php"><?php echo $more_events; ?></a></p>
+            <?php
+            }
+            ?>
         </section>
 
         <section id="projects" class="section_header">
             <h1 class="section_name"><?php echo $popular_projects ?></h1>
             <ul><?php echo projects_limited(get_popular_projects($max_shown_projects), $max_shown_projects); ?></ul>
-            <p class="more_items"><a href="projectoverview.php"><?php echo $more_projects; ?></a></p>
+            <?php
+            if (count(get_projects()) > $max_shown_projects) {
+                ?>
+                <p class="more_items"><a href="projectoverview.php"><?php echo $more_projects; ?></a></p>
+            <?php
+            }
+            ?>
         </section>
-
     </div>
 </div>
 </body>
